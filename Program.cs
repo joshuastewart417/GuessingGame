79b68d69413d3bf1800840534e156a4dabb2392c
joshuastewart417 <joshua.stewart780@gmail.com>
2021-11-16 14:50:44 -0600

    completed random int generator, multiple user attempts, and a counter for attempts

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Guess a number! Any number!");
+            Console.WriteLine("---------------------------");
+            Console.Write($"{""}");
+            int answer = int.Parse(Console.ReadLine());
+
+
+            void userQuery() {
+
+            int secretNum = 42;
+            
+
+            for(int i = 0; i < 3; i++) {
+
+            int attempt = 0;
+            attempt = Convert.ToInt32(i + 1);
+
+            if( answer != secretNum ){
+                Console.WriteLine("Attempt #" + $"{attempt} That's not it, try again!");
+                answer = int.Parse(Console.ReadLine());
+            }
+            else 
+            {
+             if( answer == secretNum ){
+                 Console.WriteLine("You got it!");
+                 Environment.Exit(0);
+             }
+            }
+        }
+    }
+         userQuery();
         }
     }
 }
